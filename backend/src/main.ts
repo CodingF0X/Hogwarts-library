392027968e9f02261fa90f56b4c2eaeed29b4adf
CodingF0X <roody9a@gmail.com>
2025-06-19 20:42:13 +0200

    added validator and cookie parser

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+import { Logger } from 'nestjs-pino';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 4000);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+
+  const conifgService = app.get(ConfigService);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  // app.useLogger(app.get(Logger));
+  app.use(cookieParser());
+
+  await app.listen(conifgService.getOrThrow('PORT'));
 }
 bootstrap();
